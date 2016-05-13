9ea6d3e7d0c3c3d5d8a659e63be4a5f294848ce0
mcfullard <mcfullard@gmail.com>
2016-05-14 01:38:02 +0200

    added different stars - the other were too big. working on listing searched trips. need to add address as property in db and trip class

--- LiftME/app/src/main/java/fnm/wrmc/nmmu/liftme/SearchResultsActivity.java
+++ LiftME/app/src/main/java/fnm/wrmc/nmmu/liftme/SearchResultsActivity.java
@@ -9,6 +9,8 @@ import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -35,11 +37,21 @@ public class SearchResultsActivity extends AppCompatActivity {
     private Trip userTrip;
     private Handler searchResultsHandler;
     private List<SearchedTrip> matchingTrips;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+        mRecyclerView = (RecyclerView) findViewById(R.id.search_results_recycler);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new SearchResultsAdapter(matchingTrips);
+        mRecyclerView.setAdapter(mAdapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
